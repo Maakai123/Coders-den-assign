20730193255cb5910200f362e6999f1a5901600d
maakai princewill <maakaiprincewill@gmail.com>
2023-07-24 18:22:46 +0100

    commit day2

---
+++ DayTwo.js
@@ -0,0 +1,14 @@
+ //1 
+ const challenge = '30 Days of JavaScript'
+
+//2
+ console.log(challenge) // 30 Days of JavaScript
+
+ //3
+
+ console.log(challenge.length) //output : 21
+//4
+ console.log(challenge.toUpperCase()) //  30 DAYS OF JAVASCRIPT
+
+ //5
+ console.log(challenge.toLowerCase()) // 30 days of javascript
