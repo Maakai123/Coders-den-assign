0bdb525da12b9d31d299d72f02a13d41a8586229
maakai princewill <maakaiprincewill@gmail.com>
2023-07-26 19:04:07 +0100

    update daytwo

--- DayTwo.js
+++ DayTwo.js
@@ -12,3 +12,34 @@
 
  //5
  console.log(challenge.toLowerCase()) // 30 days of javascript
+
+ //6
+ console.log(challenge.substring(0, 3))  // 30
+
+ 7//
+ console.log(challenge.substring(3, 20)) //Days of JavaScrip
+
+ 8//
+ console.log(challenge.includes('Script')) //true
+
+9// split into an array
+
+console.log(challenge.split()) //[ '30 Days of JavaScript' ]
+
+10//
+console.log(challenge.split(''))
+
+11//
+
+const split =  'Facebook, Google,  Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(split.split(',')) //convert to Arrays
+
+
+12//  
+
+console.log(challenge.replace('JavaScript', 'Python'))  //30 Days of Python
+
+
+
+
+
